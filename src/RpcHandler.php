18dbf55b10f823c76b8859233af59474b8a01135
he ping <847050412@qq.com>
2022-03-01 18:38:38 +0800

    wip

---
+++ src/RpcHandler.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * This file is part of SwowCloud
+ * @license  https://github.com/swow-cloud/music-server/blob/main/LICENSE
+ */
+
+declare(strict_types=1);
+
+namespace SwowCloud\RpcServer;
+
+interface RpcHandler
+{
+    /**
+     * @param \SwowCloud\RpcServer\RpcRequest $request
+     * @param null|\SwowCloud\RpcServer\RpcResponse $response
+     *
+     * @return null|\SwowCloud\RpcServer\RpcResponse
+     */
+    public function handle(RpcRequest $request, ?RpcResponse $response = null): ?RpcResponse;
+}
